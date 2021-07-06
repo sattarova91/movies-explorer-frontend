b19637e8a0ecf8997329186a805ac076e2d05d62
Anastasia Churikova <sattarova-91@inbox.ru>
2021-07-06 20:37:40 +0100

    search adaptive

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,21 +5,35 @@ import searchImg from '../../images/search-img.svg';
 function Search(props) {
   return (
     <section className="search">
-    <div className="search__container">
-      <div className="search__bar">
-        <img className="search__img" alt="поиск" src={searchImg} />
-        <input className="search__find-input" placeholder="Фильм"></input>
-        <button className="button search__find-button">Найти</button>
+      <div className="search__container">
+        <div className="search__bar">
+          <img className="search__img" alt="поиск" src={searchImg} />
+          <input className="search__find-input" placeholder="Фильм"></input>
+          <button className="button search__find-button">Найти</button>
+        </div>
+        <div className="search__settings">
+          <label className="search__switch">
+            <input type="checkbox" className="search__checkbox" id="checkbox" />
+            <span className="search__slider"></span>
+          </label>
+          <p className="search__paragraph">Короткометражки</p>
+        </div>
       </div>
-      <div className="search__settings">
+      <div className="search__container_mobile">
+        <div className="search__bar search__bar_mobile">
+          <img className="search__img" alt="поиск" src={searchImg} />
+          <input className="search__find-input" placeholder="Фильм"></input>
+          <button className="button search__find-button">Найти</button>
+        </div>
+      </div>
+      <div className="search__settings_mobile">
         <label className="search__switch">
           <input type="checkbox" className="search__checkbox" id="checkbox" />
           <span className="search__slider"></span>
         </label>
         <p className="search__paragraph">Короткометражки</p>
       </div>
-    </div>
-  </section>
+    </section>
   )
 }
 
