31670477576460f1f285d9d5f6af4a8df3b49570
Anastasia Churikova <sattarova-91@inbox.ru>
2021-07-18 15:29:16 +0100

    add filmduration

--- src/components/FilmCard/FilmCard.js
+++ src/components/FilmCard/FilmCard.js
@@ -5,13 +5,19 @@ function saveCard(evt) {
   evt.target.classList.toggle('film-card__save-button_saved');
 }
 
+function minutesToHours(allMinutes) {
+  const hours = Math.floor(allMinutes/60);
+  const minutes = allMinutes % 60;
+  return `${hours}ч ${minutes}м`;
+}
+
 function MoviesCard(props) {
   return (
     <div className="film-card" key={props.card.id}>
       <img className="film-card__img" src={'https://api.nomoreparties.co' + props.card.image.url} alt="изображенияФильма" />
       <div className="film-card__about">
         <p className="film-card__title">{props.card.nameRU}</p>
-        <p className="film-card__time">{props.card.duration}</p>
+        <p className="film-card__time">{minutesToHours(props.card.duration)}</p>
       </div>
       <button className={"button film-card__save-button" + (props.card.saved ? " hidden" : "")} onClick={saveCard}></button>
       <button className={"button film-card__detele-button" + (props.card.saved  ? ""  : " hidden")} onClick={saveCard}></button>
